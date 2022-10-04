8dd5dce70cd5afed902d5ec58ada743a633ab73f
AdityaSharma <professionaladitya484@gmail.com>
2022-10-04 22:45:21 +0530

    Commited Unity Game

---
+++ ChaosGame/Assets/Scripts/EscExit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscExit : MonoBehaviour {
+
+
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+}
