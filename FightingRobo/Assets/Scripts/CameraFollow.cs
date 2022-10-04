1c2f2bc08d4b119bae0c86cf1bca70aec2b46706
AdityaSharma <professionaladitya484@gmail.com>
2022-10-04 23:35:16 +0530

    Fighting Robo Unity Game Commited

---
+++ FightingRobo/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform Player;
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - Player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = Vector3.Lerp(transform.position, Player.position + offset, 0.1f);
+	}
+}
---
+++ FightingRobo/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour 
+{
+
+	public Slider sldr;
+	public float health = 5, damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2d(Collision2D col)
+	{
+		if(col.gameObject.tag == "BUllet")
+		{
+			health -= damage;
+			sldr.value = health;
+		}
+	}
+}
