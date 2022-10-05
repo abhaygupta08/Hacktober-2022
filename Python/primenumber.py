2f629ed9275c99a061c0a67c4c595dbc4e660a29
ADshady13 <aditya.raviswami@gmail.com>
2022-10-05 22:13:49 +0530

    prime number

---
+++ Python/primenumber.py
@@ -0,0 +1,12 @@
+num = 19
+if num > 1:
+
+    for i in range(2, int(num/2)+1):
+
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
