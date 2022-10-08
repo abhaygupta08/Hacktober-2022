77ed0d1e80dd9e7d7190e55063e9b88fd383113b
Dhruv Ranjan <97687317+dhruv18ranjan@users.noreply.github.com>
2022-10-08 17:22:23 +0530

    Update NQueen.cpp

--- CPP/NQueen.cpp
+++ CPP/NQueen.cpp
@@ -1,86 +1,149 @@
-
-
-#include <bits/stdc++.h>
-#define N 4
-using namespace std;
-
-void printSolution(int board[N][N])
+void addSolution(vector<vector<int>> &board,vector<vector<int>> &ans,int n)
 {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
-			cout << " " << board[i][j] << " ";
-		printf("\n");
-	}
+    vector<int> temp;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            temp.push_back(board[i][j]);
+        }
+    }
+    ans.push_back(temp);
+}
+bool isSafe(int row,int col,vector<vector<int>> &board,int n)
+{
+    int x=row;
+    int y=col;
+    while(y>=0)
+    {
+        if(board[x][y]==1)
+        {
+            return false;
+        }
+        y--;
+    }
+     x=row;
+     y=col;
+    while(x>=0&&y>=0)
+    {
+         if(board[x][y]==1)
+        {
+            return false;
+        }
+        y--;
+        x--;
+    }
+     x=row;
+     y=col;
+     while(x<n&&y>=0)
+    {
+         if(board[x][y]==1)
+        {
+            return false;
+        }
+        y--;
+        x++;
+    }
+    return true;
 }
 
 
-bool isSafe(int board[N][N], int row, int col)
+void solve(int col,vector<vector<int>> &ans,vector<vector<int>> &board,int n)
 {
-	int i, j;
-
-	for (i = 0; i < col; i++)
-		if (board[row][i])
-			return false;
-
-	/* Check upper diagonal on left side */
-	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-		if (board[i][j])
-			return false;
-
-	/* Check lower diagonal on left side */
-	for (i = row, j = col; j >= 0 && i < N; i++, j--)
-		if (board[i][j])
-			return false;
-
-	return true;
+    if(col==n)
+    {
+        addSolution(board,ans,n);
+        return;
+    }
+    for(int row=0;row<n;row++)
+    {
+        if(isSafe(row,col,board,n))
+        {
+            board[row][col]=1;
+            solve(col+1,ans,board,n);
+            board[row][col]=0;
+        }
+    }
+}
+vector<vector<int>> nQueens(int n)
+{
+	// Write your code here
+	vector<vector<int>> board(n,vector<int>(n,0));
+    vector<vector<int>> ans;
+    solve(0,ans,board,n);
+    return ans;
+}void addSolution(vector<vector<int>> &board,vector<vector<int>> &ans,int n)
+{
+    vector<int> temp;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            temp.push_back(board[i][j]);
+        }
+    }
+    ans.push_back(temp);
+}
+bool isSafe(int row,int col,vector<vector<int>> &board,int n)
+{
+    int x=row;
+    int y=col;
+    while(y>=0)
+    {
+        if(board[x][y]==1)
+        {
+            return false;
+        }
+        y--;
+    }
+     x=row;
+     y=col;
+    while(x>=0&&y>=0)
+    {
+         if(board[x][y]==1)
+        {
+            return false;
+        }
+        y--;
+        x--;
+    }
+     x=row;
+     y=col;
+     while(x<n&&y>=0)
+    {
+         if(board[x][y]==1)
+        {
+            return false;
+        }
+        y--;
+        x++;
+    }
+    return true;
 }
 
 
-bool solveNQUtil(int board[N][N], int col)
+void solve(int col,vector<vector<int>> &ans,vector<vector<int>> &board,int n)
 {
-	
-	if (col >= N)
-		return true;
-
-
-	for (int i = 0; i < N; i++) {
-
-		if (isSafe(board, i, col)) {
-			board[i][col] = 1;
-
-			if (solveNQUtil(board, col + 1))
-				return true;
-
-		
-			board[i][col] = 0; // BACKTRACK
-		}
-	}
-
-
-	return false;
+    if(col==n)
+    {
+        addSolution(board,ans,n);
+        return;
+    }
+    for(int row=0;row<n;row++)
+    {
+        if(isSafe(row,col,board,n))
+        {
+            board[row][col]=1;
+            solve(col+1,ans,board,n);
+            board[row][col]=0;
+        }
+    }
 }
-
-
-bool solveNQ()
+vector<vector<int>> nQueens(int n)
 {
-	int board[N][N] = { { 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 } };
-
-	if (solveNQUtil(board, 0) == false) {
-		cout << "Solution does not exist";
-		return false;
-	}
-
-	printSolution(board);
-	return true;
+	// Write your code here
+	vector<vector<int>> board(n,vector<int>(n,0));
+    vector<vector<int>> ans;
+    solve(0,ans,board,n);
+    return ans;
 }
-
-// driver program to test above function------------------contributed by dhruv ranjan
-int main()
-{
-	solveNQ();
-	return 0;
-}
-
