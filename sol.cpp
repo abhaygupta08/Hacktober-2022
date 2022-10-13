67d195ba105da45ddc5a857200d9787423f2fcd6
Sayyad Azhar <80194496+Azhar221@users.noreply.github.com>
2022-10-13 17:50:57 +0530

    Implement two stacks in an array

--- sol.cpp
+++ sol.cpp
@@ -1,203 +1,125 @@
 /*
-Level order traversal
+Implement two stacks in an array
 
-Given a binary tree, find its level order traversal.
-Level order traversal of a tree is breadth-first traversal for the tree.
+Your task is to implement  2 stacks in one array efficiently.
+
+ 
 
 Example 1:
 
 Input:
-    1
-  /   \ 
- 3     2
-Output:1 3 2
-Example 2:
+push1(2)
+push1(3)
+push2(4)
+pop1()
+pop2()
+pop2()
 
-Input:
-        10
-     /      \
-    20       30
-  /   \
- 40   60
-Output:10 20 30 40 60
+Output:
+3 4 -1
+
+Explanation:
+push1(2) the stack1 will be {2}
+push1(3) the stack1 will be {2,3}
+push2(4) the stack2 will be {4}
+pop1()   the poped element will be 3 
+from stack1 and stack1 will be {2}
+pop2()   the poped element will be 4 
+from stack2 and now stack2 is empty
+pop2()   the stack2 is now empty hence -1 .
+ 
 
 Your Task:
-You don't have to take any input. Complete the function levelOrder() that takes the root node as input parameter and returns a list of integers containing the level order traversal of the given Binary Tree.
+You don't need to read input or print anything. You are required to complete the 4 methods push1, 
+push2 which takes one argument an integer 'x' to be pushed into stack one and two and pop1, 
+pop2 which returns the integer poped out from stack one and two. 
+If no integer is present in the array return -1.
 
+ 
 
-Expected Time Complexity: O(N)
-Expected Auxiliary Space: O(N)
+Expected Time Complexity: O(1) for all the four methods.
+Expected Auxiliary Space: O(1) for all the four methods.
 
+ 
 
 Constraints:
-1 ≤ Number of nodes ≤ 105
-1 ≤ Data of a node ≤ 105
+1 <= Number of queries <= 100
+1 <= value in the stack <= 100
+The sum of elements in both the stacks < size of the given array
 
 */
-
-
-/* A binary tree Node
-
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-};
-*/
-
 
 #include<bits/stdc++.h>
 using namespace std;
 
-/*A binary tree node has data, pointer to left child
-and a pointer to right child */
+class twoStacks{
+    int *arr;
+    int size;
+    int top1,top2;
 
-struct Node{
-    int data;
-    struct Node* left;
-    struct Node* right;
+public:
+    twoStacks(int n=100){size = n; arr = new int[n]; top1 = -1; top2 = size;}
+
+    void push1(int x);
+    void push2(int x);
+    int pop1();
+    int pop2();
 };
-
-// Utility function to create a new Tree Node
-
-Node* newNode(int val){
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-
-    return temp;
-}
-
-class Solution
-{
-    public:
-    //Function to return the level order traversal of a tree.
-    vector<int> levelOrder(Node* node)
-    {
-      //Your code here
-              vector <int> res;
-        
-        //if node is null, we return the list.
-        if (node == NULL) 
-        return res;
-        
-        //creating an empty queue for level order traversal.
-        queue<Node *> q;
-    
-        q.push(node);
-    
-        while (q.empty() == false)
-        {
-            //storing front element of queue in list and removing it from queue.
-            Node *node = q.front();
-            res.push_back (node->data);
-            q.pop();
-    
-            //storing the left child of the parent node in queue.
-            if (node->left != NULL)
-                q.push(node->left);
-    
-            //storing the right child of the parent node in queue.
-            if (node->right != NULL)
-                q.push(node->right);
-        }
-        //returning the list.
-        return res;
-    }
-};
-
-/*Helper function to test mirror(). Given a binary search tree,
-print out its data elements in increasing sorted order */
-
-void inOrder(struct Node* node){
-    if(node == NULL){
-        return;
-    }
-    inOrder(node->left);
-    printf("%d",node->data);
-    inOrder(node->right);
-}
-
-// Function to Build Tree
-
-Node* buildTree(string str){
-    //corner case
-    if(str.length()==0 || str[0]=='N'){
-        return NULL;
-    }
-
-    // Creating vector of strings from input
-    // string after splitting by space
-    vector<string>ip;
-    istringstream iss(str);
-    for(string str; iss >> str; ){
-        ip.push_back(str);
-    }
-    //Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-
-    //Push the root to the queue
-    queue<Node*>queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i=1;
-    while(!queue.empty() && i<ip.size()){
-        //Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if(currVal != "N"){
-            //Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-
-            //Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if(i>=ip.size()){
-            break;
-        }
-        currVal = ip[i];
-
-        // If the right child is not null
-        if(currVal != "N"){
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-    return root;
-}
 
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        string s;
-        getline(cin,s);
-        Node* root = buildTree(s);
-        Solution ob;
-        vector<int>res=ob.levelOrder(root);
-        for(int i:res)cout<<i<<" ";
+    int T;
+    cin>>T;
+    while(T--){
+        twoStacks *sq = new twoStacks();
+
+        int  Q;
+        cin>>Q;
+        while(Q--){
+            int stack_no;
+            cin>>stack_no;
+            int QueryType = 0;
+            cin>>QueryType;
+
+            if(QueryType==1){
+                int a;
+                cin>>a;
+                if(stack_no == 1)sq->push1(a);
+                else if(stack_no == 2)sq->push2(a);
+            }else if(QueryType == 2){
+                if(stack_no == 1)cout<<sq->pop1()<<" ";
+                else if(stack_no == 2)cout<<sq->pop2()<<" ";                
+            }
+        }
         cout<<endl;
     }
-    return 0;
 }
 
+void twoStacks :: push1(int x){
+    if(top1<top2-1){
+        top1++;
+        arr[top1]=x;
+    }
+}
+
+void twoStacks :: push2(int x){
+    if(top1<top2-1){
+        top2--;
+        arr[top2]=x;
+    }
+}
+
+int twoStacks ::pop1(){
+    if(top1>=0){
+        int x = arr[top1];
+        top1--;
+        return x;
+    }else return -1;
+}
+
+int twoStacks :: pop2(){
+    if(top2<size){
+        int x = arr[top2];
+        top2++;
+        return x;
+    }else return -1;
+}
