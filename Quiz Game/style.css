6ed25cff3821008091f192279baf50db878c2afd
Aira Jena <113255984+0xaira@users.noreply.github.com>
2022-10-14 10:49:04 +0530

    Create style.css

---
+++ Quiz Game/style.css
@@ -0,0 +1,58 @@
+.content {
+margin-top: 54px;
+}
+.header {
+padding: 15px;
+position: fixed;
+top: 0;
+width: 100%;
+z-index: 9999;
+}
+.left-title {
+width: 80px;
+color: #fff;
+font-size: 18px;
+float: left;
+}
+.right-title {
+width: 150px;
+text-align: right;
+float: right;
+color: #fff;
+}
+.quiz-body {
+margin-top: 15px;
+padding-bottom: 50px;
+}
+.option-block-container {
+margin-top: 20px;
+max-width: 420px;
+}
+.option-block {
+padding: 10px;
+background: aliceblue;
+border: 1px solid #84c5fe;
+margin-bottom: 10px;
+cursor: pointer;
+}
+.result-question {
+font-weight: bold;
+}
+.c-wrong {
+margin-left: 20px;
+color: #ff0000;
+}
+.c-correct {
+margin-left: 20px;
+color: green;
+}
+.last-row {
+border-bottom: 1px solid #ccc;
+padding-bottom: 25px;
+margin-bottom: 25px;
+}
+.res-header {
+border-bottom: 1px solid #ccc;
+margin-bottom: 15px;
+padding-bottom: 15px;
+}
