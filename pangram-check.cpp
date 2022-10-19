4e9b59559bb68813fe94dc513076009661846e4c
Osama <syed.m.osama.new@gmail.com>
2022-10-20 02:30:11 +0530

    added code for pangram checker

---
+++ pangram-check.cpp
@@ -0,0 +1,31 @@
+// Concept
+// A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+// TODO
+// Given a string sentence containing only lowercase English letters,
+// return true if sentence is a pangram, or false otherwise.
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+string checkIfPangram(string sentence) {
+        set<char> st (sentence.begin(),sentence.end());
+        char alpha = 'a';
+        for(auto i: st){
+            if(i == alpha) alpha++;
+            else return "false";
+        }
+        return alpha == '{'?"true":"false";
+}
+
+int main(){
+
+    //Example string ex = "hello world" //false
+    string ex = "thequickbrownfoxjumpsoverthelazydog"; //true
+
+    cout<<checkIfPangram(ex);
+
+    return 0;
+}
