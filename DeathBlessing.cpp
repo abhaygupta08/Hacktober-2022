f31690dc3fa75c27f8df74d04ae7ed88d53bcf71
Suryash Kumar Jha <joblesscoder2468@gmail.com>
2022-10-22 12:03:48 +0530

    Codeforces Div2 Death's Blessing

---
+++ DeathBlessing.cpp
@@ -0,0 +1,44 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define int long long
+#define fo(i, n) for(int i=0;i<n;i++)
+#define deba(i, a, n) fo(i, n){cout << a[i] << " ";}
+#define pb push_back
+#define mod 1e9 +7
+#define w(x) int x; cin>>x; while(x--)
+#define endl "\n"
+#define vp(i, a, n) vi a; fo(i, n){int x; cin>>x; a.pb(x);}
+#define ap(i, a, n) fo(i, n){cin>>a[i];}
+
+typedef vector<int> vi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //*find_by_order(val);   order_of_key(val);
+
+void solution() {
+   int n;
+   cin>>n;
+   vp(i, a, n);
+   vp(i, b, n);
+int ans=0;
+sort(b.begin(), b.end());
+for(int i=0; i<n-1; i++) ans+= b[i];
+for(int i=0; i<n; i++) ans+= a[i];
+
+cout<<ans<<endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    w(t)
+    {
+        solution();
+    }
+   
+    return 0;
+}
